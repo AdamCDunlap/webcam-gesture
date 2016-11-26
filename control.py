2923def448c6ba19be25baed0327015bbcae248b
Adam Dunlap <dunlapadam@gmail.com>
2016-11-25 19:14:52 -0800

    Adding some basic computer controls

---
+++ control.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+
+import autopy
+
+def next_tab():
+    autopy.key.tap(autopy.key.K_PAGEUP, autopy.key.MOD_CONTROL)
+
+def prev_tab():
+    autopy.key.tap(autopy.key.K_PAGEDOWN, autopy.key.MOD_CONTROL)
+
+def close_tab():
+    autopy.key.tap(autopy.key.K_F4, autopy.key.MOD_CONTROL)
+
+
+def test():
+    import time
+
+    time.sleep(2)
+    next_tab()
+    time.sleep(1)
+    prev_tab()
+    time.sleep(1)
+    close_tab()
+
+if __name__ == '__main__':
+    test()
