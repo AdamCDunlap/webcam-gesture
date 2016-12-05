316ba45b40f0c3a09112ac80404094e106cadf74
Lisa Yin <lyin@g.hmc.edu>
2016-12-04 17:30:06 -0800

    Control.py now works on mac

--- control.py
+++ control.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python2
 
 import autopy
+import platform
+
+OS_MAC = 'Darwin' in platform.system()
 
 def next_tab():
-    autopy.key.tap(autopy.key.K_PAGEUP, autopy.key.MOD_CONTROL)
+    if OS_MAC:
+        autopy.key.tap(long(autopy.key.K_RIGHT),
+                       long(autopy.key.MOD_ALT)|long(autopy.key.MOD_META))
+    else:
+        autopy.key.tap(autopy.key.K_PAGEUP,
+                       autopy.key.MOD_CONTROL)
 
 def prev_tab():
-    autopy.key.tap(autopy.key.K_PAGEDOWN, autopy.key.MOD_CONTROL)
+    if OS_MAC:
+        autopy.key.tap(long(autopy.key.K_LEFT),
+                       long(autopy.key.MOD_ALT)|long(autopy.key.MOD_META))
+    else:
+        autopy.key.tap(autopy.key.K_PAGEDOWN,
+                       autopy.key.MOD_CONTROL)
 
 def close_tab():
-    autopy.key.tap(autopy.key.K_F4, autopy.key.MOD_CONTROL)
+    if OS_MAC:
+        autopy.key.tap(u'w', long(autopy.key.MOD_META))
+    else:
+        autopy.key.tap(autopy.key.K_F4, autopy.key.MOD_CONTROL)
+
+def reopen_tab():
+    if OS_MAC:
+        autopy.key.tap(u't',
+                       long(autopy.key.MOD_SHIFT)|long(autopy.key.MOD_META))
 
 def click_at_loc(x,y):
     autopy.mouse.move(x, y)
@@ -24,6 +45,10 @@ def test():
     prev_tab()
     time.sleep(1)
     close_tab()
+    time.sleep(1)
+    reopen_tab()
+    time.sleep(1)
+    click_at_loc(50,50)
 
 if __name__ == '__main__':
     test()
